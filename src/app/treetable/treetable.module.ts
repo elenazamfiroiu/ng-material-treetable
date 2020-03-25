f177a7c03ed6460f0e706d586215bc28a043cbdc
Vasile Popa <vasile.popa@endava.com>
2020-03-25 17:52:36 +0200

    Export tree table component.

--- src/app/treetable/treetable.module.ts
+++ src/app/treetable/treetable.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MatIconModule } from '@angular/material/icon';
 import { TreetableComponent } from './component/treetable.component';
-import {MatButtonModule} from '@angular/material';
-export { Node, Options} from './models';
-export {TreeService} from './services/tree/tree.service';
+import { MatButtonModule } from '@angular/material';
+export { Node, Options } from './models';
+export { TreeService } from './services/tree/tree.service';
+export { TreetableComponent } from './component/treetable.component';
 
 @NgModule({
   declarations: [
